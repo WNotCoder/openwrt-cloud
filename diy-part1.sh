10ec268ba75a750233ef1f040e34d6c31abee896
WNotCoder <30321199+WNotCoder@users.noreply.github.com>
2026-01-22 17:18:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git chenmozhijin https://github.com/chenmozhijin/luci-app-socat' >>feeds.conf.default
 echo 'src-git tailscale https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community' >>feeds.conf.default

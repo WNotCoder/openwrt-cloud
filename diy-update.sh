59e1c97cb57283b16db6ba4928964c60019e0977
WNotCoder <30321199+WNotCoder@users.noreply.github.com>
2026-04-12 10:48:42 +0800

    Update diy-update.sh to manage package feeds
    Remove specific packages and clone updated golang repository.

--- diy-update.sh
+++ diy-update.sh
@@ -1,1 +1,4 @@
 # Add some actions
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
